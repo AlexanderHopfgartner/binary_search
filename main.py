ccb28d6ede8f88462176c7a613720765b5023a9d
AlexanderHopfgartner <Alexander.Hopfgartner@outlook.com>
2023-04-28 22:29:31 +0200

    binary_search done

---
+++ main.py
@@ -0,0 +1,24 @@
+from test import ordered_list
+from math import log2
+
+
+def binary_search(searching_list: list[int], number: int):
+    current_point = int(len(searching_list)/2)
+    last_highpoint = len(searching_list)
+    last_low_point = 0
+    runtime = round(log2(len(searching_list))+0.5)
+    while searching_list[current_point] != number and runtime:
+        if number < searching_list[current_point]:
+            last_highpoint = current_point
+            current_point = int((current_point + last_low_point)/2)
+        else:
+            last_low_point = current_point
+            current_point = int((current_point + last_highpoint)/2)
+        runtime -= 1
+    if number == searching_list[current_point]:
+        return current_point
+    else:
+        return -1
+
+
+# print(ordered_list[binary_search(ordered_list, 97157)])
