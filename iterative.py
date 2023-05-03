f5a1d00f6c867ddae19f60dc7749029d5a658f5a
AlexandereHopfgartner <Alexander.Hopfgartner@outlook.com>
2023-05-03 09:04:45 +0200

    cleaned

--- iterative.py
+++ iterative.py
@@ -1,24 +1,34 @@
-from test import ordered_list
 from math import log2
 
 
 def binary_search(searching_list: list[int], number: int):
+    """Return the index of the number in the seaching_list.
+
+    If the number is not included
+    Return -1"""
+    # Evaluate the current point.
+    # Declares the last_high_point and las_low_point
     current_point = int(len(searching_list)/2)
-    last_highpoint = len(searching_list)
+    last_high_point = len(searching_list)
     last_low_point = 0
+    # Declare the runtime check
     runtime = round(log2(len(searching_list))+0.5)
+    # While the index(current_point) of the list is not the searching number AND the runtime counter is not over
+    # Search for the right index(current_point)
     while searching_list[current_point] != number and runtime:
+        # Reset the last_high_point with the current_point +1 if the number is still too low
         if number < searching_list[current_point]:
-            last_highpoint = current_point + 1
+            last_high_point = current_point + 1
             current_point = (current_point - 1 + last_low_point) // 2
+        # Reset the last_low_point with the current_point -1 if the number is still too high
         else:
             last_low_point = current_point - 1
-            current_point = (current_point + 1 + last_highpoint) // 2
+            current_point = (current_point + 1 + last_high_point) // 2
+        # -1 form the runtime per turn
         runtime -= 1
+    # Return the index(current_point) if the item on the index of the list ist the same number
     if number == searching_list[current_point]:
         return current_point
+    # Return -1 if the runtime is over cause of overflow thanks to math
     else:
         return -1
-
-
-# print(ordered_list[binary_search(ordered_list, 97157)])
--- recursive.py
+++ recursive.py
@@ -1,24 +1,28 @@
-from test import ordered_list
-from math import log2
-
-
 def binary_search(searching_list: list[int], number: int, *args):
+    """Return the index of the number in the seaching_list.
+
+    If the number is not included
+    Return -1"""
+    # Checks if it is the first time run else args will be set
     if args:
         mid = (args[0] + args[1]) // 2
-        print(mid, args)
+        # As long the low point ist smaller or equal to the high point it will search
         if args[0] <= args[1]:
+            # Return the index(mid) if the number is on the current location
             if searching_list[mid] == number:
                 return mid
+            # Checks if the index(mid) is still too low and recall self with the new mid +1 as new low
             if searching_list[mid] < number:
                 number = binary_search(searching_list, number, mid + 1, args[1])
+            # Checks if the index(mid) is still too high and recall self with the new mid -1 as new high
             elif searching_list[mid] > number:
                 number = binary_search(searching_list, number, args[0], mid - 1)
+        # Return -1 if the low is too high or the high is too low compared to the other
+        # Cause the value is not in the list
         else:
             number = -1
+    # Recall with the expected start *args
     else:
         number = binary_search(searching_list, number, 0, len(searching_list)-1)
+    # Final return
     return number
-
-
-print(binary_search(ordered_list, 42))
-
